33c09b99f6a10d4a1dad2fcd293fe75dce42f8b1
CodeLyo-ko <code_lyoko22@proton.me>
2025-02-28 09:28:54 +0200

    2015 day 18-19

---
+++ 2015/20/part1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define INPUT 36000000
+int main() {
+	int* houses = malloc(INPUT * sizeof(int));
+	for (int i = 0; i < INPUT; i++){
+		houses[i] = 10;
+	}
+	for (int i = 2; i < INPUT; i++){
+		for (int j = 0; j < INPUT; j+=i){
+			houses[j] += i * 10;
+		}
+	}
+
+	for (int i = 0; i < INPUT; i++){
+		if (houses[i] >= INPUT){
+			printf("%d\n", i);
+			break;
+		}
+	}
+	return 0;
+}
